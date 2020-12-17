42c7adc0e6d1b079d6bd781fee38fe0b0653a90f
wyb <swuwyb@163.com>
2020-12-17 14:20:55 +0800

    v1

---
+++ ClassLibrary1/BLMHelper.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Advanced_Combat_Tracker;
+
+namespace FFXIV_WYB
+{
+    public class BLMHelper : UserControl, IActPluginV1
+    {
+        private GroupBox groupBox1;
+        private CheckBox checkBox2;
+        private CheckBox checkBox1;
+        private FFXIV_ACT_Plugin.FFXIV_ACT_Plugin _ffxiv_Plugin;
+
+        #region Designer Created Code (Avoid editing)
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.checkBox2);
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Location = new System.Drawing.Point(19, 26);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "资源监视";
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(25, 49);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(107, 19);
+            this.checkBox2.TabIndex = 3;
+            this.checkBox2.Text = "Fire X监视";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(25, 24);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(89, 19);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "回蓝监视";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // BLMHelper
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "BLMHelper";
+            this.Size = new System.Drawing.Size(915, 443);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        #endregion
+        public BLMHelper()
+        {
+            InitializeComponent();
+        }
+
+        public void DeInitPlugin()
+        {
+            //_ffxivPlugin.DataSubscription.ProcessChanged -= ProcessChanged;
+        }
+
+        void IActPluginV1.InitPlugin(TabPage pluginScreenSpace, Label pluginStatusText)
+        {
+            pluginScreenSpace.Text = "黑魔助手";
+            pluginScreenSpace.Controls.Add(this);
+
+            _ffxiv_Plugin = GetFfxivPlugin();
+
+            pluginStatusText.Text = "黑魔助手启动";
+        }
+
+        /// <summary>
+        ///     取得解析插件的进程（从獭爹那里偷来的）
+        /// </summary>
+        /// <returns></returns>
+        private FFXIV_ACT_Plugin.FFXIV_ACT_Plugin GetFfxivPlugin()
+        {
+            FFXIV_ACT_Plugin.FFXIV_ACT_Plugin ffxivActPlugin = null;
+            foreach (var actPluginData in ActGlobals.oFormActMain.ActPlugins)
+                if (actPluginData.pluginFile.Name.ToUpper().Contains("FFXIV_ACT_Plugin".ToUpper()) &&
+                    actPluginData.lblPluginStatus.Text.ToUpper().Contains("FFXIV Plugin Started.".ToUpper()))
+                    ffxivActPlugin = (FFXIV_ACT_Plugin.FFXIV_ACT_Plugin)actPluginData.pluginObj;
+            return ffxivActPlugin ?? throw new Exception("找不到FFXIV解析插件");
+        }
+        }
+}
---
+++ ClassLibrary1/Common/BLMListener.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FFXIV_ACT_Plugin;
+using FFXIV_ACT_Plugin.Common;
+
+
+namespace BLMListener
+{
+    public delegate void PlayerStatsChanged();
+    class BLMListener
+    {
+        private FFXIV_ACT_Plugin.FFXIV_ACT_Plugin _ffxiv_Plugin;
+        public BLMListener(FFXIV_ACT_Plugin.FFXIV_ACT_Plugin _ffxiv_Plugin)
+        {
+            this._ffxiv_Plugin = _ffxiv_Plugin;
+            _ffxiv_Plugin.DataSubscription.PlayerStatsChanged += new PlayerStatsChangedDelegate(PlayerStatsChangedHandler);
+        }
+
+        public void PlayerStatsChangedHandler(object playerStats) {
+
+        }
+    }
+}
